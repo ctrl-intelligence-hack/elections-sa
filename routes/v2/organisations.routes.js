40f706c49bab9e3dad56bf3ce1a8427ecb1c2c78
khabubundivhu <khabubundivh@gmail.com>
2020-12-31 16:00:05 +0200

    paginate-info package installed, get organisations pagaination completed

--- routes/v2/organisations.routes.js
+++ routes/v2/organisations.routes.js
@@ -1,4 +1,6 @@
 const router = require('express').Router();
+const { query } = require('express');
+const { calculateLimitAndOffset, paginate } = require('paginate-info')
 const organisations = require('../../data/v2/organisations');
 
 /**
@@ -7,9 +9,15 @@ const organisations = require('../../data/v2/organisations');
  * @access private
  */
 router.get('/', (req, res) => {
+    const { currentPage, pageSize } = req.query;
+    const { limit, offset } = calculateLimitAndOffset(currentPage, pageSize);
+    const count = organisations.length;
+    const paginatedData = organisations.slice(offset, offset + limit);
+    const paginationInfo = paginate(currentPage, count, paginatedData);
     try {
+
         res.status(200).json({
-            organisations,
+            data: { results: paginatedData, meta: paginationInfo },
             message: 'All South Africa political parties successfully retrieved',
             success: true
         })
